806827438497d7f0dee36edaba4cb8a0874b0c9b
LBLuc <lshsu01@gmail.com>
2022-09-15 12:29:54 -0700

    finished merging functions coauthored by hannah

--- examples/depth-limiter/opine.ts
+++ examples/depth-limiter/opine.ts
@@ -1,7 +1,15 @@
-import { opine, OpineRequest, GraphQLHTTP, makeExecutableSchema, gql, readAll, buildSchema } from '../../deps.ts'
-import { depthLimiter, costLimiter } from '../../src/mod.ts'
+import {
+  opine,
+  OpineRequest,
+  GraphQLHTTP,
+  makeExecutableSchema,
+  gql,
+  readAll,
+  buildSchema,
+} from "../../deps.ts";
+import { depthLimiter, costLimiter, guardenoQL } from "../../src/mod.ts";
 
-type Request = OpineRequest & { json: () => Promise<any> }
+type Request = OpineRequest & { json: () => Promise<any> };
 
 // RUN COMMAND
 // deno run --allow-read --allow-net opine.ts
@@ -11,7 +19,7 @@ type Request = OpineRequest & { json: () => Promise<any> }
 //     posts: [Post]
 //     post(id: ID!): Post
 //   }
-  
+
 //   type Post {
 //     id: ID!
 //     title: String!
@@ -31,88 +39,191 @@ type Request = OpineRequest & { json: () => Promise<any> }
 //   },
 // };
 
-const petMixin = `
-  name: String!
-  owner: Human!
-`
+// const petMixin = `
+//   name: String!
+//   owner: Human!
+// `
 
-const schema = buildSchema(`
+// const schema = buildSchema(`
+//   type Query {
+//     user(name: String): Human
+//     version: String
+//     user1: Human
+//     user2: Human
+//     user3: Human
+//   }
+
+//   type Human {
+//     name: String!
+//     email: String!
+//     address: Address
+//     pets: [Pet]
+//   }
+
+//   interface Pet {
+//     ${petMixin}
+//   }
+
+//   type Cat {
+//     ${petMixin}
+//   }
+
+//   type Dog {
+//     ${petMixin}
+//   }
+
+//   type Address {
+//     street: String
+//     number: Int
+//     city: String
+//     country: String
+//   }
+// `);
+
+const typeDefs = gql`
   type Query {
-    user(name: String): Human
-    version: String
-    user1: Human
-    user2: Human
-    user3: Human
+    info: String!
+    feed: [Link!]!
+    link(id: ID!): Link
   }
 
-  type Human {
-    name: String!
-    email: String!
-    address: Address
-    pets: [Pet]
+  type Mutation {
+    post(url: String!, description: String!): Link!
+    updateLink(id: ID!, url: String, description: String): Link
+    deleteLink(id: ID!): Link
   }
 
-  interface Pet {
-    ${petMixin}
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
+`;
+// links stores an array of objects, each obj has 3 props (id, url, description)
+// *note: everything is stored in memory rather than a database
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
-  type Cat {
-    ${petMixin}
-  }
+// implementation of GraphQL schema
+const resolvers = {
+  Query: {
+    info: () => "This is the API of a Hackernews Clone",
+    link: (parent, args) => {
+      for (let i = 0; i < links.length; i++) {
+        if (links[i].id === args.id) {
+          return links[i];
+        }
+      }
+      return null;
+    },
+    // resolver for the feed root field (resolver must be named exactly after the corresponding field from the schema definition)
+    feed: () => links,
+  },
+  Mutation: {
+    // args represents the arguments for the operation!
+    post: (parent, args) => {
+      // generates a unique id for each link
+      let idCount = links.length;
+      // creates a new link object
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      // adds the link to the links list
+      links.push(link);
+      // returns the new link object
+      return link;
+    },
+    updateLink: (parent, args) => {
+      for (let i = 0; i < links.length; i++) {
+        if (links[i].id === args.id) {
+          if (args.url) links[i].url = args.url;
+          if (args.description) links[i].description = args.description;
+          return links[i];
+        }
+      }
+      return null;
+    },
+    deleteLink: (parent, args) => {
+      for (let i = 0; i < links.length; i++) {
+        if (links[i].id === args.id) {
+          const delLink = links[i];
+          links.splice(i, 1);
+          return delLink;
+        }
+      }
+      return null;
+    },
+  },
+  // Link resolvers are unnecessary, because the implementation is trivial!
+  // you can remove this entirely...
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  },
+};
 
-  type Dog {
-    ${petMixin}
-  }
+const dec = new TextDecoder();
 
-  type Address {
-    street: String
-    number: Int
-    city: String
-    country: String
-  }
-`);
-
-
-const dec = new TextDecoder()
-
-// const schema = makeExecutableSchema({ resolvers, typeDefs })
+const schema = makeExecutableSchema({ resolvers, typeDefs });
 // const schema = makeExecutableSchema({ typeDefs })
 
-const app = opine()
+const app = opine();
 
 app
-  .use('/graphql', async (req, res) => {
-    const request = req as Request
+  .use("/graphql", async (req, res) => {
+    const request = req as Request;
 
     request.json = async () => {
-      const rawBody = await readAll(req.raw)
-      const body = JSON.parse(dec.decode(rawBody))
+      const rawBody = await readAll(req.raw);
+      const body = JSON.parse(dec.decode(rawBody));
       const query = body.query;
       // run all queries through the depth limit function
       // if the max depth limit is exceeded, an array with error(s) will be returned
       // store the errors array in a variable
       // const error = depthLimiter(schema, query, 2);
-      
+
       // input: options object
-        // maxCost (number)
-        // mutationCost (number)
-        // objectCost (number)
-        // scalarCost (number)
-        // depthCostFactor (number)
-        // ignoreIntrospection
-      const error = costLimiter(
-        schema, 
-        query, 
-        {
-          maxCost: 18,
-          mutationCost: 5,
-          objectCost: 2,
+      // maxCost (number)
+      // mutationCost (number)
+      // objectCost (number)
+      // scalarCost (number)
+      // depthCostFactor (number)
+      // ignoreIntrospection
+      // const error = costLimiter(
+      //   schema,
+      //   query,
+      //   {
+      //     maxCost: 6,
+      //     mutationCost: 5,
+      //     objectCost: 2,
+      //     scalarCost: 1,
+      //     depthCostFactor: 2,
+      //     ignoreIntrospection: true
+      //   }
+      // );
+      // if there were no errors, return the body and let the query run
+      const error = guardenoQL(schema, query, {
+        depthLimitOptions: {
+          maxDepth: 4,
+        },
+        costLimitOptions: {
+          // maxCost: 20,
+          // mutationCost: 5,
+          // objectCost: 2,
           scalarCost: 1,
           depthCostFactor: 2,
-          ignoreIntrospection: true
-        }
-      );
-      // if there were no errors, return the body and let the query run
+          ignoreIntrospection: true,
+        },
+      });
+
       if (!error.length) {
         return body;
       } else {
@@ -120,15 +231,18 @@ app
         // send the error to the client
         return res.send(JSON.stringify(errorMessage));
       }
-    }
+    };
 
-    const resp = await GraphQLHTTP<Request>({ schema, context: (request) => ({ request }), graphiql: true })(request)
+    const resp = await GraphQLHTTP<Request>({
+      schema,
+      context: (request) => ({ request }),
+      graphiql: true,
+    })(request);
 
-    for (const [k, v] of resp.headers.entries()) res.headers?.append(k, v)
+    for (const [k, v] of resp.headers.entries()) res.headers?.append(k, v);
 
-    res.status = resp.status
-    
-    res.send(await resp.text())
-  
+    res.status = resp.status;
+
+    res.send(await resp.text());
   })
-  .listen(3000, () => console.log(`☁  Started on http://localhost:3000`))
+  .listen(3000, () => console.log(`☁  Started on http://localhost:3000`));
--- src/mod.ts
+++ src/mod.ts
@@ -1,12 +1,23 @@
-import { Source, parse, Kind, ValidationContext, GraphQLError, buildSchema, validate, specifiedRules, NullValueNode, GraphQLSchema, DefinitionNode } from '../deps.ts'
+import {
+  Source,
+  parse,
+  Kind,
+  ValidationContext,
+  GraphQLError,
+  buildSchema,
+  validate,
+  specifiedRules,
+  NullValueNode,
+  GraphQLSchema,
+  DefinitionNode,
+} from "../deps.ts";
 
 type Maybe<T> = T | null | undefined;
 
-
-// TODO: 
+// TODO:
 // Depth-limiter invokes the determineDepth function passing in each query/mutation from the queries array
 // Cost-limiter only takes in the root node and recursively determines the cost of the query/mutation of that query
-// What is the difference between these two approaches? 
+// What is the difference between these two approaches?
 
 // QUESTIONS:
 // Do we want to integrate all of our functionality into one method OR have the user invoke seperate methods
@@ -22,79 +33,122 @@ function depthLimit(maxDepth: number) {
     const fragments = getFragments(definitions);
     // console.log('fragments are: ', fragments);
     const queries = getQueriesAndMutations(definitions);
-    console.log('queries are: ', queries);
+    // console.log('queries are: ', queries);
     const queryDepths = {};
     for (const name in queries) {
       // console.log('name is: ', name);
-      queryDepths[name] = determineDepth(queries[name], fragments, 0, maxDepth, validationContext, name)
+      queryDepths[name] = determineDepth(
+        queries[name],
+        fragments,
+        0,
+        maxDepth,
+        validationContext,
+        name
+      );
     }
     //to ignore introScpect Queries from constantly spamming terminal
-    if (!Object.keys(queryDepths).includes('IntrospectionQuery')) {
-      console.log('query depths are: ', queryDepths);
+    if (!Object.keys(queryDepths).includes("IntrospectionQuery")) {
+      console.log("query depths are: ", queryDepths);
     }
     // console.log('validation context is: ', validationContext);
     return validationContext;
-  }
+  };
 }
 
 function getFragments(definitions: ReadonlyArray<DefinitionNode>) {
   return definitions.reduce((map, definition) => {
     if (definition.kind === Kind.FRAGMENT_DEFINITION) {
-      map[definition.name.value] = definition
+      map[definition.name.value] = definition;
     }
-    return map
-  }, {})
+    return map;
+  }, {});
 }
 
 // this will actually get both queries and mutations. we can basically treat those the same
 function getQueriesAndMutations(definitions) {
   return definitions.reduce((map, definition) => {
     if (definition.kind === Kind.OPERATION_DEFINITION) {
-      map[definition.name ? definition.name.value : ''] = definition
+      map[definition.name ? definition.name.value : ""] = definition;
     }
-    return map
-  }, {})
+    return map;
+  }, {});
 }
 
-function determineDepth(node, fragments, depthSoFar: number, maxDepth: number, context, operationName) {
+function determineDepth(
+  node,
+  fragments,
+  depthSoFar: number,
+  maxDepth: number,
+  context,
+  operationName
+) {
   if (depthSoFar > maxDepth) {
-    // ERROR code that wasn't working: 
+    // ERROR code that wasn't working:
     return context.reportError(
-      new GraphQLError(`'${operationName}' exceeds maximum operation depth of ${maxDepth}`, [node])
-    )
+      new GraphQLError(
+        `'${operationName}' exceeds maximum operation depth of ${maxDepth}`,
+        [node]
+      )
+    );
 
     // New error handling I added for now
     // throw 'Maximum depth exceeded!';
   }
 
   switch (node.kind) {
-    case Kind.FIELD:
-      {
-        // by default, ignore the introspection fields which begin with double underscores
-        const shouldIgnore = /^__/.test(node.name.value)
+    case Kind.FIELD: {
+      // by default, ignore the introspection fields which begin with double underscores
+      const shouldIgnore = /^__/.test(node.name.value);
 
-        if (shouldIgnore || !node.selectionSet) {
-          // console.log('shouldIgnore', shouldIgnore);
-          return 0
-        }
-        return 1 + Math.max(...node.selectionSet.selections.map(selection =>
-          determineDepth(selection, fragments, depthSoFar + 1, maxDepth, context, operationName)
-        ))
+      if (shouldIgnore || !node.selectionSet) {
+        // console.log('shouldIgnore', shouldIgnore);
+        return 0;
       }
+      return (
+        1 +
+        Math.max(
+          ...node.selectionSet.selections.map((selection) =>
+            determineDepth(
+              selection,
+              fragments,
+              depthSoFar + 1,
+              maxDepth,
+              context,
+              operationName
+            )
+          )
+        )
+      );
+    }
     case Kind.FRAGMENT_SPREAD:
-      return determineDepth(fragments[node.name.value], fragments, depthSoFar, maxDepth, context, operationName)
+      return determineDepth(
+        fragments[node.name.value],
+        fragments,
+        depthSoFar,
+        maxDepth,
+        context,
+        operationName
+      );
     case Kind.INLINE_FRAGMENT:
     case Kind.FRAGMENT_DEFINITION:
     case Kind.OPERATION_DEFINITION:
-      return Math.max(...node.selectionSet.selections.map(selection =>
-        determineDepth(selection, fragments, depthSoFar, maxDepth, context, operationName)
-      ))
+      return Math.max(
+        ...node.selectionSet.selections.map((selection) =>
+          determineDepth(
+            selection,
+            fragments,
+            depthSoFar,
+            maxDepth,
+            context,
+            operationName
+          )
+        )
+      );
     /* istanbul ignore next */
     default:
-      throw new Error('uh oh! depth crawler cannot handle: ' + node.kind)
+      throw new Error("uh oh! depth crawler cannot handle: " + node.kind);
   }
 }
-
 
 // helper functions
 function createDocument(query: string) {
@@ -105,19 +159,23 @@ function createDocument(query: string) {
 // idea:
 // main 'middelware' function, depthLimiter
 // invokes the validate function, passing in the schema, document from the query, and an array with specified rules (imported), and the invocation of the depthLimit function, passing in the person's desired maxDepth
-export function depthLimiter(schema: GraphQLSchema, query: string, maxDepth: number) {
+export function depthLimiter(
+  schema: GraphQLSchema,
+  query: string,
+  maxDepth: number
+) {
   const document = createDocument(query);
   return validate(schema, document, [...specifiedRules, depthLimit(maxDepth)]);
 }
 
 // Note: should the query itself count as an object?
 // input: options object
-  // maxCost (number)
-  // mutationCost (number)
-  // objectCost (number)
-  // scalarCost (number)
-  // depthCostFactor (number)
-  // ignoreIntrospection
+// maxCost (number)
+// mutationCost (number)
+// objectCost (number)
+// scalarCost (number)
+// depthCostFactor (number)
+// ignoreIntrospection
 // output: function that takes ValidationContext object as an arg and returns out a ValidationContext
 function costLimit(options) {
   return (validationContext) => {
@@ -126,73 +184,111 @@ function costLimit(options) {
     const queries = getQueriesAndMutations(definitions);
     const queryCostLimit = {};
     for (const name in queries) {
-      queryCostLimit[name] = determineCost(queries[name], fragments, 0, options, validationContext, name);
+      queryCostLimit[name] = determineCost(
+        queries[name],
+        fragments,
+        0,
+        options,
+        validationContext,
+        name
+      );
     }
-    console.log(queryCostLimit);
+    console.log("query costs are:", queryCostLimit);
     return validationContext;
-  } 
+  };
 }
 
-function determineCost(node, fragments, depth, options, context, operationName) {
-  
-  const {maxCost, mutationCost, objectCost, scalarCost, depthCostFactor, ignoreIntrospection} = options;
+function determineCost(
+  node,
+  fragments,
+  depth,
+  options,
+  context,
+  operationName
+) {
+  const {
+    maxCost,
+    mutationCost,
+    objectCost,
+    scalarCost,
+    depthCostFactor,
+    ignoreIntrospection,
+  } = options;
 
   let cost = scalarCost;
 
-
-  // let mutation = false;
-  // console.log('Is the name prop in node?', 'name' in node);
-  // console.log('node.name is:', node.name);
-  // console.log('node.name.value is:', node.name?.value);
   if (node.kind === Kind.OPERATION_DEFINITION) {
     cost = 0;
-    // TODO: if it's a mutation, add in the cost for mutation
-    if ('selectionSet' in node && node.selectionSet) {
+    if (node.operation === "mutation") {
+      cost = mutationCost;
+    }
+
+    if ("selectionSet" in node && node.selectionSet) {
       for (let child of node.selectionSet.selections) {
-        cost += determineCost(child, fragments, depth + 1, options, context, operationName);
+        cost += determineCost(
+          child,
+          fragments,
+          depth + 1,
+          options,
+          context,
+          operationName
+        );
       }
     }
   }
 
-  if (ignoreIntrospection && node.name !== undefined && /^__/.test(node.name?.value)) {
-    //console.log('is this reached? introspec');
+  if (
+    ignoreIntrospection &&
+    node.name !== undefined &&
+    /^__/.test(node.name?.value)
+  ) {
     return 0;
   }
 
-  // if ('operation' in node && node.operation === 'mutation') {
-  //   mutation = true;
-  //   cost = mutationCost;
-  // } 
-
-  if (node.kind !== Kind.OPERATION_DEFINITION && 'selectionSet' in node && node.selectionSet) {
-    // console.log('Is selection set reached?');
-    // console.log('The selection set is:', node.selectionSet);
-    // if (!mutation) {
-    //   cost = objectCost;
-    // }
+  if (
+    node.kind !== Kind.OPERATION_DEFINITION &&
+    "selectionSet" in node &&
+    node.selectionSet
+  ) {
     cost = objectCost;
     for (let child of node.selectionSet.selections) {
-      cost += depthCostFactor * determineCost(child, fragments, depth + 1, options, context, operationName);
+      cost +=
+        depthCostFactor *
+        determineCost(
+          child,
+          fragments,
+          depth + 1,
+          options,
+          context,
+          operationName
+        );
     }
   }
 
-  if (node.kind === Kind.FRAGMENT_SPREAD && 'name' in node) {
+  if (node.kind === Kind.FRAGMENT_SPREAD && "name" in node) {
     const fragment = fragments[node.name?.value];
-    //needs to test 
     if (fragment) {
-      cost += depthCostFactor * determineCost(fragment, fragments, depth + 1, options, context, operationName);
+      cost +=
+        depthCostFactor *
+        determineCost(
+          fragment,
+          fragments,
+          depth + 1,
+          options,
+          context,
+          operationName
+        );
     }
   }
 
-  if (cost > maxCost){
+  if (cost > maxCost) {
     return context.reportError(
-      new GraphQLError(`'${operationName}' exceeds maximum operation cost of ${maxCost}`, [node])
-    )
+      new GraphQLError(
+        `'${operationName}' exceeds maximum operation cost of ${maxCost}`,
+        [node]
+      )
+    );
   }
-
-  // console.log(`THE NODE.NAME.VALUE IS: ${node.name.value} \nTHE COST IS: ${cost}`);
-  console.log('The node.kind is:', node.kind);
-  console.log('The for this node is:', cost);
 
   return cost;
 }
@@ -201,3 +297,68 @@ export function costLimiter(schema: GraphQLSchema, query: string, options) {
   const document = createDocument(query);
   return validate(schema, document, [...specifiedRules, costLimit(options)]);
 }
+
+export function guardenoQL(schema: GraphQLSchema, query: string, options) {
+  const { depthLimitOptions, costLimitOptions } = options;
+  const document = createDocument(query);
+  // if depthLimiter is truthy AND costLimiter is truthy
+  if (depthLimitOptions && costLimitOptions) {
+    // if depthLimiter DOESN'T have maxDepth prop, throw an error
+    if (!depthLimitOptions.hasOwnProperty("maxDepth")) {
+      throw "missing max depth property on depthLimiter!";
+    }
+    if (checkCostProps(costLimitOptions)) {
+      return validate(schema, document, [
+        ...specifiedRules,
+        depthLimit(depthLimitOptions.maxDepth),
+        costLimit(costLimitOptions),
+      ]);
+    }
+  }
+  // else if depthLimiter is truthy
+  else if (depthLimitOptions) {
+    if (!depthLimitOptions.hasOwnProperty("maxDepth")) {
+      throw "missing max depth property on depthLimiter!";
+    }
+    return validate(schema, document, [
+      ...specifiedRules,
+      depthLimit(depthLimitOptions.maxDepth),
+    ]);
+  }
+  // else if costLimiter is truthy
+  else if (costLimitOptions) {
+    if (checkCostProps(costLimitOptions)) {
+      return validate(schema, document, [
+        ...specifiedRules,
+        costLimit(costLimitOptions),
+      ]);
+    }
+    // if(costErrors.length){
+    //   throw `Error with ${costErrors} prop(s) on costLimiter!`;
+    // }
+  }
+  // else error
+  else {
+    throw "missing depthLimiter & costLimiter options!";
+  }
+}
+
+//helper functions to check props on cost
+const checkCostProps = (costLimiterOptions) => {
+  const props = [
+    "maxCost",
+    "mutationCost",
+    "objectCost",
+    "scalarCost",
+    "depthCostFactor",
+    "ignoreIntrospection",
+  ];
+  const badPropsArr = props.filter(
+    (prop) => !costLimiterOptions.hasOwnProperty(prop)
+  );
+  if (badPropsArr.length) {
+    throw `Error with ${badPropsArr} prop(s) on costLimiter!`;
+  } else {
+    return true;
+  }
+};

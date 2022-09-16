ae2f35d1f1836a1e335bb018d6a8e92b555082f8
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-09-16 15:06:39 -0700

    Refactor to exclude unnecessary types. Co-authored: Jane You

--- src/protections/helper-functions.ts
+++ src/protections/helper-functions.ts
@@ -8,21 +8,19 @@ import {
 } from "../types.ts";
 
 export function getFragments(definitions: ReadonlyArray<DefinitionNode>): DefinitionNodeObject {
-  const fragmentMap: DefinitionNodeObject = {};
-  return definitions.reduce((map, definition) => {
+  return definitions.reduce((map: DefinitionNodeObject, definition) => {
     if (definition.kind === Kind.FRAGMENT_DEFINITION) {
       map[definition.name.value] = definition;
     }
     return map;
-  }, fragmentMap);
+  }, {});
 }
 
 export function getQueriesAndMutations(definitions: ReadonlyArray<DefinitionNode>): DefinitionNodeObject {
-  const queryMutationMap: DefinitionNodeObject = {};
-  return definitions.reduce((map, definition) => {
+  return definitions.reduce((map: DefinitionNodeObject, definition) => {
     if (definition.kind === Kind.OPERATION_DEFINITION) {
       map[definition.name ? definition.name.value : ""] = definition;
     }
     return map;
-  }, queryMutationMap);
+  }, {});
 }

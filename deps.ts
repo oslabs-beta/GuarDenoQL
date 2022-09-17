70ff991bfb90ccb1486eee402ad287644e89145f
finleydecker <finleydecker@gmail.com>
2022-09-17 16:03:00 -0700

    added testing for depth limiter

--- deps.ts
+++ deps.ts
@@ -13,15 +13,18 @@ export {
 
 export { opine } from 'https://deno.land/x/opine@2.2.0/mod.ts';
 export { GraphQLHTTP } from 'https://deno.land/x/gql@1.1.2/mod.ts'
-export { makeExecutableSchema } from 'https://deno.land/x/graphql_tools@0.0.2/mod.ts'
-export { gql } from 'https://deno.land/x/graphql_tag@0.0.1/mod.ts'
-export { readAll } from 'https://deno.land/std@0.148.0/streams/conversion.ts'
-export { Server } from 'https://deno.land/std@0.148.0/http/server.ts'
+export { makeExecutableSchema } from 'https://deno.land/x/graphql_tools@0.0.2/mod.ts';
+export { gql } from 'https://deno.land/x/graphql_tag@0.0.1/mod.ts';
+export { readAll } from 'https://deno.land/std@0.148.0/streams/conversion.ts';
+export { Server } from 'https://deno.land/std@0.148.0/http/server.ts';
+export { expect } from 'https://deno.land/x/expect@v0.2.10/mod.ts';
 
-export type { ValidationRule, DocumentNode, ASTVisitor } from 'https://deno.land/x/graphql_deno@v15.0.0/mod.ts';
 export type { OpineRequest } from 'https://deno.land/x/opine@2.2.0/mod.ts';
 export type { 
   DefinitionNode, 
   NullValueNode, 
   ASTNode, 
+  ValidationRule, 
+  DocumentNode, 
+  ASTVisitor
 } from 'https://deno.land/x/graphql_deno@v15.0.0/mod.ts';
